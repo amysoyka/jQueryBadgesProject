5060bcd8665044b6c6983cbc5e24da1872dde0e8
Amy Soyka <amy@darbyshire.id.au>
2017-09-03 14:48:01 +1000

    Update main.js
    Add course responses

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,6 +2,8 @@ $.ajax({
     url: 'https://www.codeschool.com/users/amysoyka.json',
     dataType: 'jsonp',
     success: function(response) {
-      // handle response
+      foreach(response.courses.completed as completed){
+        $('badges').innerHTML += '<div class="course"></div>'
+      }
     }
   });
